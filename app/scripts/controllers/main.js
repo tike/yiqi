31ddf34c58b783b92c121b4fa3c3e7081c1041c7
tike <timflex@gmx.de>
2014-07-31 21:37:01 +0200

    basic search handler

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('yiqiApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', '$log', '$http', function ($scope, $log, $http) {
+    $scope.query = 'zhu3';
+
+    $scope.search = function(query){
+        $log.info('Searching for', query);
+        var url = '//localhost:8000/search';
+        $log.debug('request url:', url);
+        $http({
+            method: 'GET',
+            url: url,
+            params: {q: query},
+          }).success(function(data){
+            $log.info('success', data);
+          }).error(function(){
+            $log.error('error fetching data.');
+          });
+        $log.debug('end of search');
+      };
+  }]);
