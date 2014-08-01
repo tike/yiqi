f472be9ac1a54c349e0994dbf56f751294aca7f0
tike <timflex@gmx.de>
2014-08-01 08:18:25 +0200

    fixed codeformatting on main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,25 +4,25 @@ angular.module('yiqiApp')
   .controller('MainCtrl', ['$scope', '$log', '$http', function ($scope, $log, $http) {
     $scope.query = 'dose';
     $scope.excuting = false;
-    $scope.message = "Search for";
+    $scope.message = 'Search for';
     $scope.querySuccessfull = false;
     $scope.foundItems = [];
 
     $scope.search = function(query){
         $log.info('Searching for', query);
         $scope.excuting = true;
-        $scope.message = "Searching for"
-        var url = '//localhost:8000/ajax/search';
+        $scope.message = 'Searching for';
+        var url = '/ajax/search';
         $log.debug('request url:', url);
         $http({
             method: 'GET',
             url: url,
             params: {q: query},
-          }).success(function(data, status, headers, config){
+          }).success(function(data, status){
             $scope.foundItems = data;
             $scope.excuting = false;
             $scope.querySuccessfull = true;
-            $scope.message = "Found " + data.length + " results for "
+            $scope.message = 'Found ' + data.length + ' results for ';
             $log.info('success', data.length, status);
           }).error(function(data, status, headers, config){
             $scope.excuting = false;
