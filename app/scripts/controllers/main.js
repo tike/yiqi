b6bd9e05e411ac0b5e1f1a9df8288558a6d23155
tike <timflex@gmx.de>
2014-08-01 06:08:07 +0200

    working search function, yikes\!/

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,20 +2,29 @@
 
 angular.module('yiqiApp')
   .controller('MainCtrl', ['$scope', '$log', '$http', function ($scope, $log, $http) {
-    $scope.query = 'zhu3';
+    $scope.query = 'dose';
+    $scope.excuting = false;
+    $scope.message = "Search for";
+    $scope.foundItems = [];
 
     $scope.search = function(query){
         $log.info('Searching for', query);
-        var url = '//localhost:8000/search';
+        $scope.excuting = true;
+        $scope.message = "Searching for"
+        var url = '//localhost:8000/ajax/search';
         $log.debug('request url:', url);
         $http({
             method: 'GET',
             url: url,
             params: {q: query},
-          }).success(function(data){
-            $log.info('success', data);
-          }).error(function(){
-            $log.error('error fetching data.');
+          }).success(function(data, status, headers, config){
+            $scope.excuting = false;
+            $scope.message = "Found " + data.length + " results for "
+            $scope.foundItems = data;
+            $log.info('success', data.length, status);
+          }).error(function(data, status, headers, config){
+            $scope.excuting = false;
+            $log.error('error', data, status, headers(), config);
           });
         $log.debug('end of search');
       };
