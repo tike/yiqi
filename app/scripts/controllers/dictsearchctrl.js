08b5e76225c37db0f6813f12d389da9e89a1947f
tike <timflex@gmx.de>
2014-08-03 00:09:37 +0200

    practically reorganised the (almost) the whole project. works now.

--- app/scripts/controllers/dictsearchctrl.js
+++ app/scripts/controllers/dictsearchctrl.js
@@ -2,67 +2,12 @@
 
 angular.module('yiqiApp')
   .controller('DictSearchCtrl', ['$scope', '$log', '$location', 'FulltextSearch', function ($scope, $log, $location, FulltextSearch) {
-    /**
-     * the current status of the search
-     *  0: now search has been made since pageload
-     *  1: user is typing a query
-     *  2: search started, result pending
-     *  3: search finished, results found
-     *  4: search finished, no results found
-     *  5: search finished, error occured
-     */
-    $scope.searchState = 0;
-    
     // The query value provided by the user
-    $scope.query = '';
-    $scope.numItems = 0;
-    
-    var promise = null;
-    
-    //messages shown to the user during the search execution cycle
-    var messages = [
-      '',
-      'Going to search for',
-      'Searching for',
-      'items found!',
-      'no matches. Sorry.',
-      'An error occured, please try again.'
-    ];
-    
-    $scope.search = function(query){
-      $scope.searchState = 2;
-      $log.debug('search pending...', $scope.searchState);
-      promise = FulltextSearch.search(query)
-      .success(function(data, status){
-          $scope.searchState = 3;
-          $scope.numItems = data.length;
-          $log.debug('found', status, data.length, $scope.searchState);
-          $location.path('/searchresults');
-        })
-      .error(function(data, status){
-        if (status === 404){
-          $scope.searchState = 4;
-          $log.debug('nothing found', status, $scope.searchState);
-          return;
-        }
-        $scope.searchState = 5;
-        $log.debug('error', status, $scope.searchState);
-      });
+    if ($scope.$parent.query === undefined){
+      $scope.query = '';
+    }
+    $scope.search = function(){
+      $location.path('/searchresults/').search({query: $scope.query });
     };
-    
-    $scope.watchSearchState = function(){
-      $scope.searchState = 1;
-    };
-    
-    $scope.getUserMessage = function(){
-      return messages[$scope.searchState];
-    };
-    
-  }])
-  .controller('ResultItemCtl', ['$scope', '$log', function($scope, $log){
-    $log.info('resItemCtl');
-  }])
-  .controller('TranslationCtl', ['$scope', '$log', function($scope, $log){
-    $log.info('TransCtl');
   }]);
 
--- app/scripts/controllers/searchresults.js
+++ app/scripts/controllers/searchresults.js
@@ -2,17 +2,61 @@
 
 angular.module('yiqiApp')
   .controller('SearchResultsCtrl', ['$scope', '$log', '$routeParams', 'FulltextSearch', function ($scope, $log, $routeParams, FulltextSearch) {
-    $log.debug('query', $routeParams);
-    if ($routeParams.query !== undefined){
-      FulltextSearch.search($routeParams.query);
-    }
-    $scope.resp = FulltextSearch.getResult();
+    /**
+     * the current status of the search
+     *  0: now search has been made since pageload
+     *  1: user is typing a query
+     *  2: search started, result pending
+     *  3: search finished, results found
+     *  4: search finished, no results found
+     *  5: search finished, error occured
+     */
+    $scope.searchState = 0;
     
-    $scope.languages = [
-      {short: 'en', 'long': 'english'},
+    //messages shown to the user during the search execution cycle
+    var messages = [
+      '',
+      'Going to search for',
+      'Searching for',
+      'items found!',
+      'no matches. Sorry.',
+      'An error occured, please try again.'
+    ];
+    var promise = null;
+    $scope.query = '';
+    $scope.found = [];
+    
+    $scope.search = function(query){
+      promise = FulltextSearch.search(query)
+      .success(function(data, status){
+          $scope.searchState = 3;
+          $scope.found = data;
+          $log.debug('found', status, data.length, $scope.searchState);
+        })
+      .error(function(data, status){
+        if (status === 404){
+          $scope.searchState = 4;
+          $log.debug('nothing found', status, $scope.searchState);
+          return;
+        }
+        $scope.searchState = 5;
+        $log.debug('error', status, $scope.searchState);
+      });
+    };
+    
+    $scope.languages = [ 'en', 'fr', 'de'];
+  /*    {short: 'en', 'long': 'english'},
       {short: 'de', 'long': 'deutsch'},
       {short: 'fr', 'long': 'francais'},
-    ];
+    ]; */
     $scope.selectedLang = '';
-
+    
+    $log.debug('Meine query', $routeParams);
+    $log.debug('Meine query', $routeParams.query);
+    if ($routeParams.query !== undefined){
+      $scope.searchState = 2;
+      $scope.query = $routeParams.query;
+      $scope.search($routeParams.query);
+    }
+  
   }]);
